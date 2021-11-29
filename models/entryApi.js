558f785e689babf6fcf9d1059ab089adb2155ee7
RUben <rubenlage12@gmail.com>
2021-11-29 18:39:02 +0100

    Querys 50%

--- models/entryApi.js
+++ models/entryApi.js
@@ -1,13 +1,7 @@
 const querys = require('../utils/querys')
+const pool = require('../utils/postgreConnection')
 // Conecta con la base de datos de Postgre
 
-const { Pool } = require('pg');
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'CodeJobs',
-    password: 'root'
-})
 
 // Obtener todos los usuarios Registrados. Exclusivo del admin.
 const getAllUsuarios = async () => {
@@ -26,11 +20,12 @@ const getAllUsuarios = async () => {
 }
 
 
-const insertUsuario = async () =>{
+const insertUsuario = async (entry) =>{
     let client, result;
+    const {nombre, email, contraseña,administrador} = entry;
     try{
         client = await pool.connect(); // Espera a abrir conexion
-        const data = await client.query(querys.insertUsuarios)
+        const data = await client.query(querys.insertUsuarios, [nombre, email, contraseña,administrador])
         result = data.rows
     }catch(err){
         console.log(err);
@@ -41,20 +36,9 @@ const insertUsuario = async () =>{
     return result
 }
 
-const insertUsuario = async () =>{
-    let client, result;
-    try{
-        client = await pool.connect(); // Espera a abrir conexion
-        const data = await client.query(querys.insertUsuarios)
-        result = data.rows
-    }catch(err){
-        console.log(err);
-        throw err;
-    }finally{
-        client.release();    
-    }
-    return result
-}
 
-getAllUsuarios()
+// getAllUsuarios()
+// .then(data=>console.log(data))
+
+insertUsuario()
 .then(data=>console.log(data))
