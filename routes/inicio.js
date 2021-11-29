b474d485ceb265f542ec0857f514188f48558823
Francisco Javier <fjfernandezsa@gmail.com>
2021-11-29 22:01:55 +0100

    addPruebaScrap

--- routes/inicio.js
+++ routes/inicio.js
@@ -1,5 +1,6 @@
 const vistasBasicas = require('../controllers/inicio');
 const routes = require('express').Router();
+const prueba = require('../controllers/controllerApi');
 
 // -------- Rutas para la vistas básicas del CLIENTE -----------
 
@@ -8,6 +9,7 @@ routes.get('/login', vistasBasicas.login);
 routes.get('/register', vistasBasicas.signin);
 routes.get('/profile', vistasBasicas.profile)
 routes.get('/favorites', vistasBasicas.favorites)
+routes.get('/prueba', prueba.prueba)
 
 // ----------- Rutas para las vistas del ADMIN --------------
 
