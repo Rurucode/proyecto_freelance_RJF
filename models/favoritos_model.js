d55e52bad6b82ef77dcdc79005d1d7df0e2d6622
jjcasco99 <juanjocasco28@gmail.com>
2021-11-29 15:36:41 +0100

    añadida la conexion con mongo y el esquema de favoritos

---
+++ models/favoritos_model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const favoriteSchema = {
+    title: { 
+        type: String, 
+        required: true 
+    },
+    price: { 
+        type: Number, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    url:{
+        type: String,
+        required: true 
+    }
+};
+// Crear el esquema
+const favoritos = mongoose.Schema(favoriteSchema);
+// Crear el modelo
+const Favoritos = mongoose.model('Favoritos', favoritos);
+
+module.exports = Favoritos;
