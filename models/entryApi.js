19402e094ed2edfbf38d7a82c3d98dd022687f06
Ruben <rubenlage12@gmail.com>
2021-11-30 19:29:07 +0100

    insert usuarios arreglado

--- models/entryApi.js
+++ models/entryApi.js
@@ -4,43 +4,58 @@ const pool = require('../utils/postgreConnection')
 
 
 // Obtener todos los usuarios Registrados. Exclusivo del admin.
-const getAllUsuarios = async () => {
-    let client,result;
-    try{
+const allUsuarios = async () => {
+    let client, result;
+    try {
         client = await pool.connect(); // Espera a abrir conexion
         const data = await client.query(querys.allUsuarios)
         result = data.rows
-    }catch(err){
+    } catch (err) {
         console.log(err);
         throw err;
-    }finally{
-        client.release();    
+    } finally {
+        client.release();
     }
     return result
 }
 
-
-const insertUsuario = async (entry) =>{
+const selectUsuario = async (id_user) => {
     let client, result;
-    const {nombre, email, contraseña} = entry;
-    try{
+    try {
         client = await pool.connect(); // Espera a abrir conexion
-        const data = await client.query(querys.insertUsuarios, [nombre, email, contraseña])
+        const data = await client.query(querys.selectUsuario, [id_user])
         result = data.rows
-    }catch(err){
+    } catch (err) {
         console.log(err);
         throw err;
-    }finally{
-        client.release();    
+    } finally {
+        client.release();
     }
     return result
 }
 
 
-// getAllUsuarios()
+const insertUsuario = async (entry) => {
+    let client, result;
+    const {nombre, email, contraseña } = entry;
+    try {
+        client = await pool.connect(); // Espera a abrir conexion
+        const data = await client.query(querys.insertUsuario, [nombre, email, contraseña])
+        result = data.rows
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        client.release();
+    }
+    return result
+}
+
+//Pruebas
+// allUsuarios()
 // .then(data=>console.log(data))
 
-let newEntry = {nombre:'jager',email:'jagger@gmail.com',contraseña:'1234'};
+let newEntry = { nombre: "jager", email: "jagger@gmail.com", contraseña: "1234" };
 
 insertUsuario(newEntry)
 .then(data=>console.log(data))
