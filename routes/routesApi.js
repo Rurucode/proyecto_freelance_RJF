6d9dd66c7aeb10ae97cff2297c6d4d07f2ace1d9
jjcasco99 <juanjocasco28@gmail.com>
2021-12-05 12:10:37 +0100

    creado el fichero para las rutas de la api

--- routes/routesApi.js
+++ routes/routesApi.js
@@ -2,6 +2,25 @@ const funcionesApi = require('../controllers/controllerApi');
 const routes = require('express').Router();
 
 
+// -------- Rutas para la vistas básicas del CLIENTE -----------
+
+routes.post('/user', funcionesApi.recogerOfertas);
+routes.put('/user', funcionesApi.recogerOfertas);
+routes.delete('/user', funcionesApi.recogerOfertas);
+routes.post('/login', funcionesApi.recogerOfertas);
+routes.post('/logout', funcionesApi.recogerOfertas);
 routes.get('/search', funcionesApi.recogerOfertas);
+routes.post('/favorites', funcionesApi.recogerOfertas);
+routes.delete('/favorites', funcionesApi.recogerOfertas);
+
+
+// ----------- Rutas para las vistas del ADMIN --------------
+
+routes.post('/ads', funcionesApi.recogerOfertas);
+routes.put('/ads', funcionesApi.recogerOfertas);
+routes.delete('/ads', funcionesApi.recogerOfertas);
+
+
+
 
 module.exports = routes;
