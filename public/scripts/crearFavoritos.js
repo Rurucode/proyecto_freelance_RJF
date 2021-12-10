35b81b5185a7994b984b3e0f181e2c68b16efb01
RUben <rubenlage12@gmail.com>
2021-12-10 09:35:53 +0100

    1.1

--- public/scripts/crearFavoritos.js
+++ public/scripts/crearFavoritos.js
@@ -1,1 +1,9 @@
 const fav = getElementById("contenedorOfertas")
+
+
+const favs = document.querySelectorAll(".classInpu");
+    favs.forEach(fav => {
+        fav.addEventListener("click", (event) => {
+            event.target.parentElement
+        })
+    })
